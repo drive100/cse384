56801e20275c640411641dddc5b1392fcbba7763
yguo108 <yguo108@syr.edu>
2017-03-05 15:21:35 -0500

    added return for the last changes

--- hw5.c
+++ hw5.c
@@ -10,5 +10,7 @@ int main(int args, char* argv[])
 	{
 		printf("Usage: This program will replicate and monitor the");
 		printf(" file that is passed into the program as an argument\n");
+		return EXIT_SUCCESS;
 	}
+
 }
